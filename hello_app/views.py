68b5354bc2644116e8de6fb5f0417106eca3dd88
Gourav Bhardwaj <AZ_November2018@outlook.com>
2020-12-24 13:11:08 -0500

    15th

--- hello_app/views.py
+++ hello_app/views.py
@@ -4,7 +4,12 @@ from . import app
 
 @app.route("/")
 def home():
-    return render_template("home.html") 
+#    return render_template("home.html")
+    return render_template(
+        "hello_there.html",
+        name=Gourav,
+        date=datetime.now()
+    ) 
 
 @app.route("/about/")
 def about():
